985d1b2b58e73a2b0830db22aace7ce1fe67b064
Pt Emmbp <ptemmbp@gmail.com>
2025-11-27 14:36:36 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/promo/layout.tsx
+++ src/app/promo/layout.tsx
@@ -55,28 +55,11 @@ export default function PromoLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="id" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body
-        className={cn(
-          'min-h-screen bg-secondary/30 font-body antialiased'
-        )}
-      >
-        <FirebaseProvider>
-            <LandingPageHeader />
-            <main>{children}</main>
-            <LandingPageFooter />
-            <FloatingWhatsApp />
-            <Toaster />
-        </FirebaseProvider>
-      </body>
-    </html>
+    <div className={cn('min-h-dvh bg-secondary/30')}>
+        <LandingPageHeader />
+        <main>{children}</main>
+        <LandingPageFooter />
+        <FloatingWhatsApp />
+    </div>
   );
 }
